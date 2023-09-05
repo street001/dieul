dff2b72e7773368c679896ffead6575661deac52
street001 <adiop6225@gmail.com>
2023-09-05 15:53:03 +0200

    pay1

---
+++ app/config.php
@@ -0,0 +1,10 @@
+<?php
+define('BXC_USER', 'StreetShop33');
+define('BXC_PASSWORD', '$2y$10$V8iNI2VrxScdYtg.mUa/GOpJ6xmPJ4s/fx6EdnSRJpZywuOvtZuHm');
+define('BXC_DB_NAME', 'shop');
+define('BXC_DB_USER', 'street');
+define('BXC_DB_PASSWORD', '**Ordinateur12');
+define('BXC_DB_HOST', '62.4.16.83');
+define('BXC_DB_PORT', '3306');
+define('BXC_URL', 'http://street-shop.cc/');
+?>
